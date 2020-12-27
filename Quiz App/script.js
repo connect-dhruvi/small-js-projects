b5019261d1545ca3dbd9f275df7d7adefaf884dc
LAPTOP-HEI0V66S\conne <connect.dhruvi@gmail.com>
2020-12-26 22:16:01 -0500

    Quiz app structure added

---
+++ Quiz App/script.js
@@ -0,0 +1,50 @@
+const quizData =[
+    {
+        question:"Who invented React JS Framework? ",
+        a:"Google",
+        b:"Microsoft",
+        c:"Facebook",
+        d:"Oracle",
+        correct:"c"
+    },
+    {
+        question:"what is the most use programming language?",
+        a:"Javascript",
+        b:"Python",
+        c:"Java",
+        d:"C++",
+        correct:"a"
+    },
+    {
+        question:"A good algorithm must be",
+        a:"Detailed",
+        b:"Replacable",
+        c:"simple",
+        d:"Open-ended",
+        correct:"a"
+    },
+    {
+        question:"which of the following is example of binary",
+        a:"true",
+        b:"false",
+        c:"boolean",
+        d:"0",
+        correct:"d"
+    },
+    {
+        question:"which port number is used by FTP protocol?",
+        a:"80",
+        b:"21",
+        c:"64",
+        d:"0",
+        correct:"b"
+    },
+    {
+        question:"what year javascript was launched?",
+        a:"1999",
+        b:"1986",
+        c:"1995",
+        d:"2002",
+        correct:"c"
+    },
+];
