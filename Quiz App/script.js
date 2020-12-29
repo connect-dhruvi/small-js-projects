5150cb644861ff7c7ac939f68961137d2f5b5d8d
LAPTOP-HEI0V66S\conne <connect.dhruvi@gmail.com>
2020-12-28 22:32:08 -0500

    quiz app completed

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -48,44 +48,74 @@ const quizData = [{
     },
 ];
 
-var a_text="";
-var b_text="";
-var c_text="";
-var d_text="";
-var current = 0; 
+var a_text = "";
+var b_text = "";
+var c_text = "";
+var d_text = "";
+var current = 0;
+var btnSubmit = "";
+var btnAnswer = "";
+var result = 0;
 
-var funcXyz = function xyz(){
-    var text =123;
-    var alertWarning = function(text){
-        alert(text);
-    }
-    return{
-        alertWarning: alertWarning,
-        errorWarning: function(text){
-
-        }
-    }
-}();
 
 function loadQuiz() {
     const currentData = quizData[current];
-    console.log(currentData.question);
-    
-    let element_question = document.getElementById("question");
-    debugger
+    var element_question = document.getElementById("question");
     element_question.innerText = currentData.question;
-
-    quizData.map((e => {
-
-    }))
+    inputA.checked = false;
+    inputB.checked = false;
+    inputC.checked = false;
+    inputA.checked = false;
 
     a_text.innerText = currentData.a;
     b_text.innerText = currentData.b;
     c_text.innerText = currentData.c;
     d_text.innerText = currentData.d;
 
-    current++;
 };
+
+function showResultEvent() {
+    var elems = document.getElementsByClassName('quiz-container');
+    for (var i = 0; i < elems.length; i += 1) {
+        elems[i].style.display = 'none';
+    }
+    // document.getElementsByClassName("quiz-container")[0].style.display = 'none';
+    console.log(result +"")
+    elResult.innerText ="Your Result is " + result;
+    document.getElementsByClassName("result-container")[0].style.display = 'block';
+
+}
+
+function addSubmitEvent() {
+    btnSubmit.addEventListener('click', () => {
+        console.log(current, quizData.length)
+          {
+            var radios = document.getElementsByTagName('input');
+            var value;
+            for (var i = 0; i < radios.length; i++) {
+                if (radios[i].type === 'radio' && radios[i].checked) {
+                    // get value, set checked flag or do whatever you need to
+                    value = radios[i].value;
+                    if (value == quizData[current].correct) {
+                        result++;
+                        debugger
+                    }
+                }
+            }
+            if (!value) {
+                alert("Select one answer to go ahead");
+            }
+           
+        }
+        if (current >= quizData.length - 1) {
+            alert("The quiz has been finished");
+            showResultEvent();
+        }
+        current++;
+        loadQuiz();
+    });
+
+}
 
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
@@ -93,5 +123,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
     b_text = document.getElementById("b_text");
     c_text = document.getElementById("c_text");
     d_text = document.getElementById("d_text");
+    btnSubmit = document.getElementById("submit");
+    inputA = document.getElementById("a");
+    inputB = document.getElementById("b");
+    inputC = document.getElementById("c");
+    inputD = document.getElementById("d");
+    btnAnswer = document.getElementsByName("answer");
+    elInput = document.getElementsByTagName("input");
+    document.getElementsByClassName("result-container")[0].style.display = 'none';
+    elResult = document.getElementById("result");
     loadQuiz();
+    addSubmitEvent();
+
 });
